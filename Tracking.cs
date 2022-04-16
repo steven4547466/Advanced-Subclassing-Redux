ada4076eeaaafad1a8c2c99cc3dcdb139dd6bd61
Steven Webster <steven@is-just-a.dev>
2022-04-16 12:00:25 -0400

    Not needed

--- Tracking.cs
+++ Tracking.cs
@@ -10,7 +10,5 @@ namespace AdvancedSubclassingRedux
         public static Dictionary<Player, Subclass> PlayersWithClasses = new Dictionary<Player, Subclass>();
         public static Dictionary<Player, PlayerSnapshot> PlayerSnapshots = new Dictionary<Player, PlayerSnapshot>();
         public static Dictionary<Player, Dictionary<Ability, DateTime>> PlayerAbilityCooldowns = new Dictionary<Player, Dictionary<Ability, DateTime>>();
-
-        public static List<string> CassieSkips = new List<string>();
     }
 }
